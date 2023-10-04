d28fad461b32f47ddca8ce889a6c86188d791812
WeslleyFranca <4weslleyfranca4@gmail.com>
2023-10-03 23:30:35 -0300

    picture upgrade

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ function toggleMode() {
     const img = document.querySelector("#profile img")
 
     if (html.classList.contains('light')) {
-        img.setAttribute('src', "./assets/avatarw-light.png")
+        img.setAttribute('src', "./assets/picture-light.png")
     } else {
-        img.setAttribute('src', "./assets/avatarw.png")
+        img.setAttribute('src', "./assets/picture.png")
     }
 }
